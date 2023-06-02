b9f5aaa905f89d569fde86bbbb323945ff173eb0
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-02 17:52:16 +0300

    feat: done task
    create  small CLI application without any external dependencies that expects the user to enter a few words or numbers separated by a space. Next, the program should ask how to sort the user's input

--- 01_CLI_Interactive Sort/app.js
+++ 01_CLI_Interactive Sort/app.js
@@ -5,15 +5,21 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+// get data from user
 rl.question(
   'Enter few words or numbers separated by space (If You want to exit, enter "exit"): ',
   (answer) => {
     if (answer.trim() === 'exit') {
       rl.close();
-    } else if (answer.trim() === '') {
-      rl.prompt();
     } else {
+      // parse input to array
       const inputArray = answer.split(' ');
+      // select only string from array
+      const stringArray = inputArray.filter((element) => isNaN(element));
+      // select only numbers from array
+      const numbersArray = inputArray.filter(Number);
+
+      // ask to chose how to process the data
       console.log(
         'Chose what operation to do with words and numbers (If You want to exit, enter "exit"). \n' +
           '1. Sort words alphabetically \n' +
@@ -23,6 +29,7 @@ rl.question(
           '5. Show only unique words \n' +
           '6. Display only unique values from the set of words and numbers entered'
       );
+      // repeat asking how to process the data, until user close the app
       rl.setPrompt('\nEnter option number (1-6):');
       rl.prompt();
       rl.on('line', (input) => {
@@ -30,35 +37,72 @@ rl.question(
           rl.close();
         } else if (input.trim() === '1') {
           console.log('Here is your words sorted alphabetically:');
-          console.log(inputArray.sort());
+          // simple js sort will sort words alphabetically
+          console.log(stringArray.sort());
           rl.prompt();
         } else if (input.trim() === '2') {
           console.log('Here is your numbers from lesser to greater:');
+          // correcting sort for numbers
+          console.log(
+            numbersArray.sort((a, b) => {
+              return a - b;
+            })
+          );
           rl.prompt();
         } else if (input.trim() === '3') {
+          3;
           console.log('Here is your numbers from bigger to smaller:');
+          // correcting sort for numbers in descend order
+          console.log(
+            numbersArray.sort((a, b) => {
+              return b - a;
+            })
+          );
           rl.prompt();
         } else if (input.trim() === '4') {
           console.log(
             'Here is your words in ascending order by number of letters in the word:'
           );
+          // add 'length' param to sort by number of letters
+          console.log(
+            stringArray.sort((a, b) => {
+              return a.length - b.length;
+            })
+          );
           rl.prompt();
         } else if (input.trim() === '5') {
           console.log('Here is unique words:');
+          // filter will check if its only one occurrence of value in the array
+          console.log(
+            // use for array excluded from numbers
+            stringArray.filter((value, index, array) => {
+              return array.indexOf(value) === index;
+            })
+          );
           rl.prompt();
         } else if (input.trim() === '6') {
           console.log(
             'Here is only unique values from the set of words and numbers entered:'
           );
+          // filter will check if its only one occurrence of value in the array
+          console.log(
+            // use for whole input array
+            inputArray.filter((value, index, array) => {
+              return array.indexOf(value) === index;
+            })
+          );
           rl.prompt();
         } else {
-          console.log('Enter "exit" to close the program');
+          // if input not 'exit' and incorrect
+          console.log('( Enter "exit" to close the program )');
+          rl.prompt();
         }
       });
     }
   }
 );
 
+// exit program any time readline process is stopped
 rl.on('close', () => {
   console.log('\nGoodbye. Have a great day!');
   process.exit(0);
