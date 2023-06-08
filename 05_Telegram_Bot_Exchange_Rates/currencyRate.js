c01a1da0cfe63fbf7b894acf4f23b02c7a7f6566
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-08 20:03:52 +0300

    feat: node cache for monobank api response
    method that get data from monobank api now save response in cache result fixing to many requests error

--- 05_Telegram_Bot_Exchange_Rates/currencyRate.js
+++ 05_Telegram_Bot_Exchange_Rates/currencyRate.js
@@ -1,5 +1,8 @@
 // axios
 const axios = require('axios');
+// node cache
+const NodeCache = require('node-cache');
+const myCache = new NodeCache({ stdTTL: 120, checkperiod: 240 });
 
 async function getPrivatCurrencyRate(currency) {
   // currency 'EUR' or 'USD' to get answers for different currencies
@@ -27,20 +30,60 @@ async function getMonobankCurrencyRate(currency) {
   // Monobank use ISO 4217 codes: USD - 840; EUR - 978; UAH - 980
   const codeA = currency === 'USD' ? 840 : 978;
   let response;
+  let success;
   await axios({
     method: 'get',
     url: 'https://api.monobank.ua/bank/currency',
-  }).then((apiResponse) => {
-    const data = apiResponse.data;
-    for (i in data) {
-      if (data[i].currencyCodeA === codeA && data[i].currencyCodeB === 980) {
-        response =
-          `Обмінний курс в Монобанк (${currency}): \n` +
-          ` Купівля: ${data[i].rateBuy} (UAH)\n` +
-          ` Продаж: ${data[i].rateSell} (UAH)\n`;
+  })
+    .then((apiResponse) => {
+      const data = apiResponse.data;
+      for (i in data) {
+        // construct cache for further use
+        if (data[i].currencyCodeA === 840 && data[i].currencyCodeB === 980) {
+          success = myCache.set('USD', data[i]);
+        }
+        if (data[i].currencyCodeA === 978 && data[i].currencyCodeB === 980) {
+          success = myCache.set('EUR', data[i]);
+        }
+        // simple answer
+        if (data[i].currencyCodeA === codeA && data[i].currencyCodeB === 980) {
+          response =
+            `Обмінний курс в Монобанк (${currency}): \n` +
+            ` Купівля: ${data[i].rateBuy} (UAH)\n` +
+            ` Продаж: ${data[i].rateSell} (UAH)\n`;
+        }
       }
-    }
-  });
+    })
+    .catch((error) => {
+      if (currency === 'USD') {
+        const data = myCache.get('USD');
+        if (data === undefined) {
+          response =
+            `Обмінний курс в Монобанк (${currency}): \n` +
+            ` Неможливо отримати дані\n`;
+        } else {
+          response =
+            `Обмінний курс в Монобанк (${currency}): \n` +
+            ` Купівля: ${data.rateBuy} (UAH)\n` +
+            ` Продаж: ${data.rateSell} (UAH)\n`;
+        }
+      } else if (currency === 'EUR') {
+        const data = myCache.get('EUR');
+        if (data === undefined) {
+          response =
+            `Обмінний курс в Монобанк (${currency}): \n` +
+            ` Неможливо отримати дані\n`;
+        } else {
+          response =
+            `Обмінний курс в Монобанк (${currency}): \n` +
+            ` Купівля: ${data.rateBuy} (UAH)\n` +
+            ` Продаж: ${data.rateSell} (UAH)\n`;
+        }
+      } else {
+        console.log(error);
+        response = `Обмінний курс в Монобанк (${currency}):\n Невідома помилка\n`;
+      }
+    });
 
   return response;
 }
