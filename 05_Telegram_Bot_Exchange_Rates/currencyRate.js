76bcc19da1a60f585b50f66f7620c703bda4eb00
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-08 19:05:06 +0300

    feat: monobank api currency rate
    add method to get monobank currency rate for USD and EUR

--- 05_Telegram_Bot_Exchange_Rates/currencyRate.js
+++ 05_Telegram_Bot_Exchange_Rates/currencyRate.js
@@ -11,6 +11,7 @@ async function getPrivatCurrencyRate(currency) {
     const data = apiResponse.data;
     for (i in data) {
       if (data[i].ccy === currency) {
+        // TODO fix slice in currency rate
         response =
           `Обмінний курс в Приват (${currency}): \n` +
           ` Купівля: ${data[i].buy.slice(0, -3)} (UAH)\n` +
@@ -22,4 +23,27 @@ async function getPrivatCurrencyRate(currency) {
   return response;
 }
 
-module.exports = { getPrivatCurrencyRate };
+async function getMonobankCurrencyRate(currency) {
+  // currency 'EUR' or 'USD' to get answers for different currencies
+  // Monobank use ISO 4217 codes: USD - 840; EUR - 978; UAH - 980
+  const codeA = currency === 'USD' ? 840 : 978;
+  let response;
+  await axios({
+    method: 'get',
+    url: 'https://api.monobank.ua/bank/currency',
+  }).then((apiResponse) => {
+    const data = apiResponse.data;
+    for (i in data) {
+      if (data[i].currencyCodeA === codeA && data[i].currencyCodeB === 980) {
+        response =
+          `Обмінний курс в Монобанк (${currency}): \n` +
+          ` Купівля: ${data[i].rateBuy} (UAH)\n` +
+          ` Продаж: ${data[i].rateSell} (UAH)\n`;
+      }
+    }
+  });
+
+  return response;
+}
+
+module.exports = { getPrivatCurrencyRate, getMonobankCurrencyRate };
