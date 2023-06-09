ba8ad8ae625fac7a363c8d667a478554d40dc450
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-09 15:36:54 +0300

    feat: read all files to array
    add method to read all files words into one array.
    
    method for creating array of paths was deleted

--- 06_Instagram_Giveaway/app.js
+++ 06_Instagram_Giveaway/app.js
@@ -1,10 +1,29 @@
-const { readFile } = require('fs');
+const { readFileSync } = require('fs');
 
-function getFilesPaths() {
-  let filesPaths = [];
-  for (i = 0; i < 20; i++) {
-    const path = './static/out' + i;
-    filesPaths.push(path);
-  }
-  return filesPaths;
+// measure elapsed time
+let startTime, endTime;
+function start() {
+  startTime = performance.now();
 }
+function end() {
+  endTime = performance.now();
+  const timeDiff = (endTime - startTime) / 1000;
+  console.log(`\nElapsed time - ${timeDiff} seconds`);
+}
+
+function getArrayOfWords() {
+  let words = [];
+  for (i = 0; i < 20; i++) {
+    const path = './static/out' + i + '.txt';
+    const data = readFileSync(path).toString().split('\n');
+    words = words.concat(data);
+  }
+  return words;
+}
+
+start();
+
+let words = getArrayOfWords();
+console.log(words.length);
+
+end();
