4d16c68568cea51be338d6d9a2ba2f219b576c52
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-13 18:51:11 +0300

    feat: same vacations days check
    add check for user to have same vacations days multiple times in original json

--- 07_Grouping_Vacations/app.js
+++ 07_Grouping_Vacations/app.js
@@ -3,12 +3,6 @@ const axios = require('axios');
 // write file
 const { writeFileSync } = require('fs');
 
-class User {
-  constructor(userId, userName, vacations) {
-    (this.userId = userId), this.userName;
-  }
-}
-
 axios({
   method: 'get',
   url: 'https://jsonbase.com/sls-team/vacations',
@@ -16,6 +10,7 @@ axios({
   let users = [];
   const responseData = response.data;
 
+  // loop through original json
   for (i = 0; i < responseData.length; i++) {
     let user = {
       userId: responseData[i].user._id,
@@ -28,10 +23,22 @@ axios({
       ],
     };
 
+    // check if new json already contain user
     let foundIndex = users.findIndex((item) => item.userId === user.userId);
     if (foundIndex > -1) {
-      users[foundIndex].vacations.push(user.vacations[0]);
+      // user already exists in result array
+      // check if original json have a multiple entries of same vacations days
+      let found = users[foundIndex].vacations.find(
+        (vacation) =>
+          vacation.startDate === user.vacations[0].startDate &&
+          vacation.endDate === user.vacations[0].endDate
+      );
+      if (!found) {
+        // user already have same vacations days
+        users[foundIndex].vacations.push(user.vacations[0]);
+      }
     } else {
+      // user not exists in result array
       users.push(user);
     }
   }
